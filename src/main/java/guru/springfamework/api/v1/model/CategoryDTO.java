8c1301e7fadc97ffadccd41027cd222f4bb198fc
Jean Griesbacher <jnsmplcpt@gmail.com>
2020-05-13 23:47:26 +0200

    category service

--- src/main/java/guru/springfamework/api/v1/model/CategoryDTO.java
+++ src/main/java/guru/springfamework/api/v1/model/CategoryDTO.java
@@ -1,8 +1,9 @@
 package guru.springfamework.api.v1.model;
 
-/**
- * Created by jt on 9/24/17.
- */
+import lombok.Data;
+
+
+@Data
 public class CategoryDTO {
     private Long id;
     private String name;
